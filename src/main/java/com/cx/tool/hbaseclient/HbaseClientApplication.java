6cc7371d5cb3b23a60baa29af5ae17b652f0b357
cx_ <15900901092@163.com>
2019-09-10 15:51:22 +0800

    创建项目

---
+++ src/main/java/com/cx/tool/hbaseclient/HbaseClientApplication.java
@@ -0,0 +1,13 @@
+package com.cx.tool.hbaseclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HbaseClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HbaseClientApplication.class, args);
+    }
+
+}
