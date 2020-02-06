fb0d6965eb93619f00a04bd25bc52df959f0f877
cx_ <15900901092@163.com>
2020-02-06 16:13:17 +0800

    0.05版本新增 1.根据表名，列族名，列名查询数据 2.根据表名，列族名查询数据

--- src/main/java/com/cx/tool/hbaseclient/HbaseClientApplication.java
+++ src/main/java/com/cx/tool/hbaseclient/HbaseClientApplication.java
@@ -1,6 +1,8 @@
 package com.cx.tool.hbaseclient;
 
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.cx.tool.hbaseclient.connection.AlHbaseConnection;
 import com.cx.tool.hbaseclient.operating.AlHbaseAddOperating;
 import com.cx.tool.hbaseclient.operating.AlHbaseQueryOperating;
@@ -11,6 +13,7 @@ import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.*;
 
 import java.io.IOException;
+import java.util.List;
 
 
 public class HbaseClientApplication {
@@ -18,8 +21,10 @@ public class HbaseClientApplication {
     public static void main(String[] args) throws IOException {
 
         AlHbaseConnection.createConnection("hb-proxy-pub-bp14m3b9dkuimkd34-master2-001.hbase.rds.aliyuncs.com:2181,hb-proxy-pub-bp14m3b9dkuimkd34-master1-001.hbase.rds.aliyuncs.com:2181,hb-proxy-pub-bp14m3b9dkuimkd34-master3-001.hbase.rds.aliyuncs.com:2181");
-        String  phto = AlHbaseQueryOperating.selectByQualifierData("merchantphoto", "merchantphotofamily", "photo", "98001");
-        System.out.println(phto);
+        List<JSONObject> resultList = AlHbaseQueryOperating.selectByTableNameDataAll("userpaycommon", "userpaycommonfamily");
+        for (JSONObject st : resultList){
+            System.out.println(st);
+        }
     }
 
 }
--- src/main/java/com/cx/tool/hbaseclient/operating/AlHbaseQueryOperating.java
+++ src/main/java/com/cx/tool/hbaseclient/operating/AlHbaseQueryOperating.java
@@ -7,9 +7,12 @@ import org.apache.hadoop.hbase.Cell;
 import org.apache.hadoop.hbase.CellUtil;
 import org.apache.hadoop.hbase.client.Get;
 import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.client.ResultScanner;
 import org.apache.hadoop.hbase.client.Table;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 阿里 hbase 查询
@@ -71,5 +74,55 @@ public class AlHbaseQueryOperating {
 
         table.close();
         return jsonObject;
+    }
+
+    /**
+     * 表名列族名列名查询所有的数据
+     * @param tableName 表名
+     * @param family 列族名
+     * @param qualifier 列名
+     * @return List<String>
+     * @throws IOException
+     */
+    public static List<String> selectByTableNameDataAll(String tableName,String family,String qualifier) throws IOException {
+        Table table = AlHbaseConnection.getTable(tableName);
+        ResultScanner resultScanner = table.getScanner(family.getBytes(),qualifier.getBytes());
+        List<String> returnResult = new ArrayList<String>();
+        for (Result result: resultScanner){
+            if(result.rawCells().length > 0){
+                returnResult.add(new String(result.getValue(family.getBytes(), qualifier.getBytes()), "UTF-8"));
+            }
+        }
+        table.close();
+        return returnResult;
+    }
+
+    /**
+     * 表名列族名查询所有的数据
+     * @param tableName 表名
+     * @param family 列族名
+     * @return List<JSONObject> key :列名 val:列对应的值
+     * @throws IOException
+     */
+    public static List<JSONObject> selectByTableNameDataAll(String tableName,String family) throws IOException {
+        Table table = AlHbaseConnection.getTable(tableName);
+        ResultScanner resultScanner = table.getScanner(family.getBytes());
+        List<JSONObject> returnResult = new ArrayList<JSONObject>();
+        for (Result result: resultScanner){
+            Cell[] cells = result.rawCells();
+            if(cells.length > 0){
+                JSONObject jsonObject = new JSONObject();
+                for (Cell cell : cells) {
+                    String qualifier = new String(CellUtil.cloneQualifier(cell), "UTF-8");
+                    String value = new String(CellUtil.cloneValue(cell), "UTF-8");
+                    if (StringUtils.isNotBlank(value) && !value.equals("null")) {
+                        jsonObject.put(qualifier, value);
+                    }
+                }
+                returnResult.add(jsonObject);
+            }
+        }
+        table.close();
+        return returnResult;
     }
 }
